589aa670c4739f38f9b6f4010141a0d84e537a3c
erislandio.soares <erislandio.soares@acct.global>
2020-10-13 22:16:28 -0300

    config huskey dep

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,6 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400
       }
@@ -14,6 +14,17 @@ describe('Login router', () => {
     const httpRequest = {
       body: {
         password: 'any_password'
+      }
+    }
+    const httpdResponse = sut.route(httpRequest)
+    expect(httpdResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 400 if no password is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'teste@teste.com'
       }
     }
     const httpdResponse = sut.route(httpRequest)
